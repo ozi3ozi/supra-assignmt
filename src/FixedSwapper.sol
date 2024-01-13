5b38adf78ba71c4c16c0aaef7660236279779f43
Othmane <my.learn.email@proton.me>
2024-01-13 07:36:42 -0500

    updating contracts

--- src/FixedSwapper.sol
+++ src/FixedSwapper.sol
@@ -2,50 +2,37 @@
 pragma solidity >=0.8.23;
 
 import "@openzeppelin/contracts/utils/Context.sol";
-
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
-interface IERC20 {
-    function totalSupply() external view returns (uint);
-
-    function balanceOf(address account) external view returns (uint);
-
-    function transfer(address recipient, uint amount) external returns (bool);
-
-    function allowance(address owner, address spender) external view returns (uint);
-
-    function approve(address spender, uint amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
-}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FixedSwapper is Context {
 /**
- * Fixed exchange rate of A to B in % in the form of
- * 1 B = A * fxdXchgRateAtoB/100 and 1 A = B * 100 / fxdXchgRateAtoB
- * i.e. 50 would exchange 1 A for 0.5 B
+ * Fixed exchange rate From A to B.
+ * We use the smallest bits based on the tokens' respective decimals.
+ * i.e. If the rate from A to B is 2, then 1 * 10**A_decimals = 2 * 10**B_decimals
+ * Which gives the following:
+ * decimalAdjustedRate = rate * 10**B_decimals / 10**A_decimals
+ * amountOfB = amountOfA * decimalAdjustedRate
+ * amountOfA = amountOfB / decimalAdjustedRate 
  */
-uint16 public immutable FXD_XCHG_RATE_A_TO_B;
-IERC20 public immutable tokenA;
-IERC20 public immutable tokenB;
+uint public immutable FXD_XCHG_RATE_A_TO_B;
+ERC20 public immutable tokenA;
+ERC20 public immutable tokenB;
 
 // constructor
     /**
-     * @param _fxdXchgRateAtoB Fixed exchange rate of A to B in % in the form of 1 A = B * fxdXchgRateAtoB/100
+     * @dev 
+     * @param _tokenA Token A
+     * @param _tokenB Token B
+     * @param _fxdXchgRateAtoB Fixed exchange rate of A to B. Must be greater than 0. i.e. rate of 2 means 1 * 10**A_decimals = 2 * 10**B_decimals
+     * the _fxdXchgRateAtoB is used to get adjusted rate based on tokens' decimals
      */
-    constructor(address _tokenA, address _tokenB, uint16 _fxdXchgRateAtoB) {
+    constructor(address _tokenA, address _tokenB, uint _fxdXchgRateAtoB) {
         require(_tokenA != address(0) && _tokenB != address(0), "zero address");
         require(_fxdXchgRateAtoB > 0, "zero exchange rate");
 
-        tokenA = IERC20(_tokenA);
-        tokenB = IERC20(_tokenB);
-        FXD_XCHG_RATE_A_TO_B = _fxdXchgRateAtoB;
+        tokenA = ERC20(_tokenA);
+        tokenB = ERC20(_tokenB);
+        FXD_XCHG_RATE_A_TO_B = _fxdXchgRateAtoB * 10**tokenB.decimals() / 10**tokenA.decimals();
     }
 // events
     event SwappedAToB(address indexed sender, uint amountIn, uint amountOut);
@@ -62,7 +49,7 @@ IERC20 public immutable tokenB;
     function swapExactAToB(uint256 _amountIn) external notZeroAddress {
         require(senderHasEnoughBalanceOf(tokenA, _amountIn), "insufficient balance");
         require(senderGaveEnoughAllowanceOf(tokenA, _amountIn), "insufficient allowance");
-
+        
         uint256 _amountOut = getTokenBAmountFromA(_amountIn);
         require(contractHasEnoughReservesOf(tokenB, _amountOut), "insufficient reserves");
 
@@ -130,11 +117,11 @@ IERC20 public immutable tokenB;
     }
 
     function getTokenBAmountFromA(uint256 _amountA) public view returns (uint256 _amountB) {
-        _amountB = _amountA * FXD_XCHG_RATE_A_TO_B / 100;
+        _amountB = _amountA * FXD_XCHG_RATE_A_TO_B;
     }
 
     function getTokenAAmountFromB(uint256 _amountB) public view returns (uint256 _amountA) {
-        _amountA = _amountB * 100 / FXD_XCHG_RATE_A_TO_B;
+        _amountA = _amountB / FXD_XCHG_RATE_A_TO_B;
     }
 
     function safeTransferFrom(
